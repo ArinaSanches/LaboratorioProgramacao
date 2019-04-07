558b31adf12a57f3974b16aa9a667051bec419c1
ArinaSanches <sanchesarina96@gmail.com>
2019-04-06 21:45:50 -0300

    [+]Adição do Quicksort com geração do vetor aleatório e pivo aleatório

---
+++ QuicksotRand/cmake-build-debug/CMakeFiles/QuicksotRand.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuicksotRand.dir/main.cpp.obj"
+  "QuicksotRand.pdb"
+  "QuicksotRand.exe"
+  "QuicksotRand.exe.manifest"
+  "libQuicksotRand.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuicksotRand.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

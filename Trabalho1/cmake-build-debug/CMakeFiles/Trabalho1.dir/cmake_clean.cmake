130c9767a30ad4d98230072e5eef9f36774b2a2e
ArinaSanches <sanchesarina96@gmail.com>
2019-04-06 21:48:12 -0300

    [+]Adição do arquivo do trabalho 1

---
+++ Trabalho1/cmake-build-debug/CMakeFiles/Trabalho1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trabalho1.dir/main.cpp.obj"
+  "Trabalho1.pdb"
+  "Trabalho1.exe"
+  "Trabalho1.exe.manifest"
+  "libTrabalho1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trabalho1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

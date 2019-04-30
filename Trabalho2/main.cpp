5501ab690740fcfd1ddea3d94585a6b5e5fc2adb
ArinaSanches <sanchesarina96@gmail.com>
2019-04-30 20:36:37 -0300

    [+]Adição do algoritmo KMP.

--- Trabalho2/main.cpp
+++ Trabalho2/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+
+void ImprimirVetor(int* vector){
+    cout << "{ ";
+    while(*vector != -1){
+        cout << *vector << " ";
+        vector ++;
+    }
+    cout << "}" << endl;
+    cout << endl << endl;
+}
+
 void buscarForcaBruta(const char *texto, const char * padrao, int *saida){
 
     const char *pos_texto = texto;
@@ -30,27 +41,80 @@ void buscarForcaBruta(const char *texto, const char * padrao, int *saida){
     *saida = -1;
 }
 
-void ImprimirVetor(int* vector){
-    cout << "{ ";
-    while(*vector != -1){
-        cout << *vector << " ";
-        vector ++;
+void calcularPi(const char * padrao, int * pi){
+    pi[0] = 0;
+    int k, l;
+    for(int i = 1; padrao[i] != '\0'; i++){
+        k = i - 1;
+        for(;;){
+            l = pi[k];
+            if(l == 0){
+                if(padrao[0] == padrao[i]){
+                    pi[i] = 1;
+                    break;
+                }else{
+                    pi[i] = 0;
+                    break;
+                }
+            }else{
+                if(padrao[i] == padrao[l]){
+                    pi[i] = l + 1;
+                    break;
+                }else{
+                    k = l - 1;
+                }
+
+            }
+        }
     }
-    cout << "}" << endl;
-    cout << endl << endl;
+}
+
+void algoritmoKMP(const char *texto, const char *padrao, int *saida){
+
+    int *pi = new int[sizeof(padrao)]();
+    calcularPi(padrao, pi);
+    int i = 0, j = 0;
+
+    while(texto[i] != '\0'){
+
+        if(texto[i] != padrao[j]){
+            if(j == 0){
+                i++;
+            }else{
+                j = pi[(j-1)];
+            }
+        }else{
+            if(padrao[j + 1] == '\0'){
+                *saida = i - j;
+                saida ++;
+                j = pi[j];
+                i++;
+            }else{
+                i++;
+                j++;
+            }
+        }
+    }
+
+    *saida = -1;
+    free(pi);
 }
 
 
 int main() {
 
-    const char *texto =  "ABDABCDABCBABCC";
-    const char *padrao = "ABC";
+    //const char *texto =  "ABDABCDABCBABCC";
+    //const char *padrao = "ABC";
 
-    int *saida =  new int[60];
+    const char *padrao = "ABA";
+    const char *texto = "ABACABADABACABACABAC";
+    /*const char *padrao = "AAA";
+    const char *texto = "AAAAAAAAAAAAAAAAAAAA";*/
 
-    buscarForcaBruta(texto, padrao, saida);
+    int *saida = new int[sizeof(texto)]();
+
+    algoritmoKMP(texto, padrao, saida);
     ImprimirVetor(saida);
-
     return 0;
 }
 
